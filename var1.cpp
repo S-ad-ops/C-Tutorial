2ec9bffa23f30a784536c9c69790f36ab32dae86
soham-pawar <sohamrpawar.2018@gmail.com>
2026-01-30 11:17:14 +0000

    var

---
+++ var1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int var1 = 10; // Local variable
+    int var2 = 20;
+    float var3 = 5.5; // Corrected: use a different variable name
+    char c = 'A'; // Local variable
+    cout << "Value of var1: " << var1 << "\nValue of var2 is: " << var2 << endl;
+    cout << "\nValue of var3: " << var3 << "\nValue of c is: " << c << endl;
+    return 0;
+}
---
+++ var2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool flag = true; // Local variable
+    double var4 = 15.75; // Local variable
+    
+
+    return 0;
+}
